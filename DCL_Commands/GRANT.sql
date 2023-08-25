b8bdd3154af35e742f9fd3839f874e3318201d02
Dhanushkumar <130890976+M-S-Dhanushkumar@users.noreply.github.com>
2023-08-25 15:06:35 +0530

    Add files via upload

---
+++ DCL_Commands/GRANT.sql
@@ -0,0 +1,7 @@
+--Grant: It is used to give user access privileges to a database.
+Syntax:GRANT SELECT, UPDATE ON MY_TABLE TO SOME_USER, ANOTHER_USER;  
+
+Example:
+GRANT SELECT, UPDATE
+ON Student
+TO Director;
