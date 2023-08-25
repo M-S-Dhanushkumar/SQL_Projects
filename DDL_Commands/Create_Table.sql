a3eccc1421dd992bca88f8cf4ba8f13af3b10291
Dhanushkumar <130890976+M-S-Dhanushkumar@users.noreply.github.com>
2023-08-25 12:51:05 +0530

    Add files via upload

---
+++ DDL_Commands/Create_Table.sql
@@ -0,0 +1,17 @@
+Syntax:
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+
+
+Example:
+CREATE TABLE Persons (
+     PersonID int,
+     LastName varchar(255),
+     FirstName varchar(255),
+     Address varchar(255),
+     City varchar(255)
+);
