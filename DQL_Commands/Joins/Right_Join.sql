0762ce6ea0d983e2575041b49ee24b14e0123274
Dhanushkumar <130890976+M-S-Dhanushkumar@users.noreply.github.com>
2023-09-01 13:18:29 +0530

    Update Right_Join.sql

--- DQL_Commands/Joins/Right_Join.sql
+++ DQL_Commands/Joins/Right_Join.sql
@@ -1,1 +1,14 @@
+--Right Join returns all records from right side of the table and matches the records from the left side of the table.
 
+
+SELECT table1.column1,table1.column2,table2.column1,....
+FROM table1 
+RIGHT JOIN table2
+ON table1.matching_column = table2.matching_column;
+
+
+--Example
+SELECT Student.NAME,StudentCourse.COURSE_ID 
+FROM Student
+RIGHT JOIN StudentCourse 
+ON StudentCourse.ROLL_NO = Student.ROLL_NO;
